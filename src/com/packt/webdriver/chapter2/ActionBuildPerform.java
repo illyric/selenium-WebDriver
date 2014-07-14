4ac7c5d983b8c3e611012b6363a8407f5f8e2b6a
Ilir Kosumi <ilirkosumi@gmail.com>
2014-07-14 14:41:22 +0200

    added class ActionBuildPerform.java ;

---
+++ src/com/packt/webdriver/chapter2/ActionBuildPerform.java
@@ -0,0 +1,42 @@
+package com.packt.webdriver.chapter2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class ActionBuildPerform {
+	public static void main(String[] args){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///C:/Users/ilirk/Desktop/8850OS_Code/Chapter%202/HTML/Selectable.html");
+		List<WebElement> elements = driver.findElements(By.tagName("li"));
+		
+		//Creating actions 
+		Actions builder = new Actions(driver);
+		builder.keyDown(Keys.CONTROL)
+			.click(elements.get(0))
+			.click(elements.get(2))
+			.click(elements.get(4))
+		.keyUp(Keys.CONTROL);		
+		
+		//create composite action
+		Action compositeAction = builder.build();
+		
+		//perform the composite action
+		compositeAction.perform();
+		
+		/*This can also be achieved using directly
+		 * builder.perform();
+		 * The perform() method calls internally 
+		 * the build() method
+		 * */
+		
+	}
+
+}
