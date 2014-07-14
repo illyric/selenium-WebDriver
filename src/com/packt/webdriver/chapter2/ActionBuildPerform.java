ac5c12ff31563a820e3702b9cb3ce34c242fa2bc
Ilir Kosumi <ilirkosumi@gmail.com>
2014-07-14 16:38:31 +0200

    modified ActionBuildPerform.java & added a test class -TestClass.java

--- src/com/packt/webdriver/chapter2/ActionBuildPerform.java
+++ src/com/packt/webdriver/chapter2/ActionBuildPerform.java
@@ -1,5 +1,6 @@
 package com.packt.webdriver.chapter2;
 
+import java.awt.EventQueue;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -12,10 +13,37 @@ import org.openqa.selenium.interactions.Actions;
 
 
 public class ActionBuildPerform {
+	
+	public ActionBuildPerform(){
+		
+	}
+	
+	public static void goToGoogle(WebDriver driver){
+		driver.get("http://www.google.com");
+	}
+	
+	
+	
+	
 	public static void main(String[] args){
 		WebDriver driver = new FirefoxDriver();
+		
 		driver.get("file:///C:/Users/ilirk/Desktop/8850OS_Code/Chapter%202/HTML/Selectable.html");
-		List<WebElement> elements = driver.findElements(By.tagName("li"));
+		List<WebElement> elements = driver.findElements(By.tagName("li"));		
+		
+		//Tread to redirect to google after 5 seconds (just for fun)
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(5000);
+					//WebDriver driver = new FirefoxDriver();
+					goToGoogle(com.packt.webdriver.chapter2.ActionBuildPerform.driver);
+					System.out.println("It worked!");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 		//Creating actions 
 		Actions builder = new Actions(driver);
---
+++ src/com/packt/webdriver/chapter2/TestClass.java
@@ -0,0 +1,12 @@
+package com.packt.webdriver.chapter2;
+
+import javax.security.auth.login.Configuration;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println(Configuration.getConfiguration());
+
+	}
+
+}
